74f612d26a61dfeeb91c298d6a58cc9886f96ffc
Rupal Radadia <rupal.radadia@gmail.com>
2023-10-05 14:48:17 -0500

    react-router

---
+++ react-router/src/app.jsx
@@ -0,0 +1,7 @@
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
+
+export function App() {
+  return <></>;
+}
